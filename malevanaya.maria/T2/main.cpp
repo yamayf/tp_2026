e565f656357ee60f9bc6a73e1becb1db50b0fb1f
winkttt <mariamalevanaya@icloud.com>
2026-05-12 00:53:45 +0300

    вспомогательная функция+цикл в мейн

--- malevanaya.maria/T2/main.cpp
+++ malevanaya.maria/T2/main.cpp
@@ -4,14 +4,32 @@
 #include <algorithm>
 #include "data_struct.hpp"
 
+//helper
+static void skipToEndOfRecord(std::istream& in) {
+    in.clear();
+    char c = '\0';
+    while (in.get(c)) {
+        if (c == ':') {
+            char next = '\0';
+            if (in.get(next)) {
+                if (next == ')') return;
+                in.putback(next);
+            }
+        }
+    }
+}
+
 int main() {
     std::vector<DataStruct> data;
-
-    std::copy(
-        std::istream_iterator<DataStruct>(std::cin),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(data)
-    );
+    DataStruct ds;
+    while (!std::cin.eof()) {
+        if (std::cin >> ds) {
+            data.push_back(ds);
+        }
+        else if (!std::cin.eof()) {
+            skipToEndOfRecord(std::cin);
+        }
+    }
 
     std::sort(data.begin(), data.end(), compareDataStruct);
 
