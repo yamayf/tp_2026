390c6f57b9e5c24890d0ea62ea244fea14b213d1
a-sik <andreyskor07@gmail.com>
2026-06-04 22:24:35 +0300

    Fixed content

--- skorohod.andrey/T2/main.cpp
+++ skorohod.andrey/T2/main.cpp
@@ -10,9 +10,7 @@
 int main()
 {
   using asik::DataStruct;
-
   std::vector<DataStruct> data;
-
   while (!std::cin.eof())
   {
     if (!std::cin)
@@ -21,21 +19,14 @@ int main()
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       continue;
     }
-
-    std::copy(
-    std::istream_iterator<DataStruct>(std::cin),
-    std::istream_iterator<DataStruct>(),
-    std::back_inserter(data));
+    std::copy(std::istream_iterator<DataStruct>(std::cin),
+      std::istream_iterator<DataStruct>(),
+      std::back_inserter(data));
   }
-
   std::sort(data.begin(), data.end());
-
   std::cout << "DataStruct:\n";
-  std::copy(
-    std::begin(data),
+  std::copy(std::begin(data),
     std::end(data),
-    std::ostream_iterator< DataStruct >(std::cout, "\n")
-  );
-
+    std::ostream_iterator< DataStruct >(std::cout, "\n"));
   return 0;
 }
